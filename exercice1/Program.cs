0b43ba3582ccee1403377fa1b4fbec329c0c8cc3
erwme <erwme@LAPTOP-UB3AHV80>
2024-10-03 11:10:47 +0200

    Ajoutez des fichiers projet.

---
+++ exercice1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom = "menez";
+            string prenom = "erwan ";
+            int age = 23;
+
+            Console.WriteLine("Bonjour " + prenom + nom + " vous avez " + age + " ans");
+            Console.ReadLine();
+        }
+    }
+}
